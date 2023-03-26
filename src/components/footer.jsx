4c310747ba5e1aad697b8b97af5defba081c3d75
ArjunKSoni <aksoni0520@gmail.com>
2023-03-26 12:38:16 +0530

    signup

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -6,70 +6,70 @@ const Footer = () => {
   return (
     <div>
 
-<footer className="bg-dark text-center text-white">
-  <div className="container p-4">
-    <section className="">
-      <form action="">
-        <div className="row d-flex justify-content-center">
-          <div className="col-auto">
-            <p className="pt-2">
-              <strong>Sign up for our newsletter</strong>
+      <footer className="bg-dark text-center text-white">
+        <div className="container p-4">
+          <section className="">
+            <form action="">
+              <div className="row d-flex justify-content-center">
+                <div className="col-auto">
+                  <p className="pt-2">
+                    <strong>Sign up for our newsletter</strong>
+                  </p>
+                </div>
+                <div className="col-md-5 col-12">
+                  <div className="form-outline form-white mb-4">
+                    <input type="email" id="form5Example21" placeholder='Email address' className="form-control" />
+                  </div>
+                </div>
+                <div className="col-auto">
+                  <button type="submit" className="btn btn-outline-light mb-4">
+                    Subscribe
+                  </button>
+                </div>
+              </div>
+            </form>
+          </section>
+          <section className="mb-4">
+            <Link className="btn btn-outline-light btn-floating m-1" to="#!" role="button"
+            ><i className="fab fa-facebook-f"></i
+            ></Link>
+            <Link className="btn btn-outline-light btn-floating m-1" to="#!" role="button"
+            ><i className="fab fa-twitter"></i
+            ></Link>
+            <Link className="btn btn-outline-light btn-floating m-1" to="#!" role="button"
+            ><i className="fab fa-google"></i
+            ></Link>
+            <Link className="btn btn-outline-light btn-floating m-1" to="#!" role="button"
+            ><i className="fab fa-instagram"></i
+            ></Link>
+            <Link className="btn btn-outline-light btn-floating m-1" to="#!" role="button"
+            ><i className="fab fa-linkedin-in"></i
+            ></Link>
+            <Link className="btn btn-outline-light btn-floating m-1" to="#!" role="button"
+            ><i className="fab fa-github"></i
+            ></Link>
+          </section>
+          <section className="mb-4">
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt distinctio earum
+              repellat quaerat voluptatibus placeat nam, commodi optio pariatur est quia magnam
+              eum harum corrupti dicta, aliquam sequi voluptate quas.
             </p>
-          </div>
-          <div className="col-md-5 col-12">
-            <div className="form-outline form-white mb-4">
-              <input type="email" id="form5Example21" placeholder='Email address' className="form-control" />
+          </section>
+          <section className="">
+            <div className="row">
             </div>
-          </div>
-          <div className="col-auto">
-            <button type="submit" className="btn btn-outline-light mb-4">
-              Subscribe
-            </button>
-          </div>
+
+          </section>
+
         </div>
-      </form>
-    </section>
-    <section className="mb-4">
-      <Link className="btn btn-outline-light btn-floating m-1" to="#!" role="button"
-        ><i className="fab fa-facebook-f"></i
-      ></Link>
-      <Link className="btn btn-outline-light btn-floating m-1" to="#!" role="button"
-        ><i className="fab fa-twitter"></i
-      ></Link>
-      <Link className="btn btn-outline-light btn-floating m-1" to="#!" role="button"
-        ><i className="fab fa-google"></i
-      ></Link>
-      <Link className="btn btn-outline-light btn-floating m-1" to="#!" role="button"
-        ><i className="fab fa-instagram"></i
-      ></Link>
-      <Link className="btn btn-outline-light btn-floating m-1" to="#!" role="button"
-        ><i className="fab fa-linkedin-in"></i
-      ></Link>
-      <Link className="btn btn-outline-light btn-floating m-1" to="#!" role="button"
-        ><i className="fab fa-github"></i
-      ></Link>
-    </section>
-    <section className="mb-4">
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt distinctio earum
-        repellat quaerat voluptatibus placeat nam, commodi optio pariatur est quia magnam
-        eum harum corrupti dicta, aliquam sequi voluptate quas.
-      </p>
-    </section>
-    <section className="">
-      <div className="row">
-      </div>
 
-    </section>
+        <div className="text-center p-3">
+          © 2020 Copyright:
+          <Link className="text-white" to="/">Farm Expert</Link>
+        </div>
+      </footer>
 
-  </div>
-
-  <div className="text-center p-3">
-    © 2020 Copyright:
-    <Link className="text-white" to="/">Krishi Samadhan</Link>
-  </div>
-</footer>
-      
     </div>
   )
 }
--- src/components/registerUser.jsx
+++ src/components/registerUser.jsx
@@ -2,7 +2,39 @@ import React from 'react'
 
 const RegisterUser = () => {
   return (
-    <div>registerUser</div>
+    <div className='register'>
+      <div class="box">
+        <div class="image"><img src="leaf.jpg" alt="" class="leaf" />
+          <h1>Sign-In</h1></div>
+        <form>
+          <div class="mb-3">
+            <label for="inputName" class="form-label">Name</label>
+            <input type="text" class="form-control" id="exampleInputname" aria-describedby="emailHelp" placeholder="Enter your Name" />
+          </div>
+
+          <div class="mb-3">
+            <label for="exampleInputEmail1" class="form-label">Email address</label>
+            <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" />
+          </div>
+          <div class="mb-3">
+            <label for="exampleInputNumber" class="form-label">Phone Number</label>
+            <input type="number" class="form-control" id="exampleInputnumbers" />
+          </div>
+          <div class="mb-3">
+            <label for="exampleInputddress1" class="form-label">Address (Optional)</label>
+            <input type="password" class="form-control" id="exampleInputPassword1" />
+          </div>
+          <div class="mb-3">
+            <label for="exampleInputPassword1" class="form-label">Password</label>
+            <input type="password" class="form-control" id="exampleInputPassword1" />
+            <div id="passwordHelp" class="form-text">Use a strong Password</div>
+          </div>
+          <div class="butt">
+            <button type="submit" class="botton">Sign-In</button>
+          </div>
+        </form>
+      </div>
+    </div>
   )
 }
 
