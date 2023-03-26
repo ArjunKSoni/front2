e1d7c6e00ec8fc9141ae1a3716694e504c25971e
ArjunKSoni <aksoni0520@gmail.com>
2023-03-26 23:29:18 +0530

    disease added

--- src/components/disease.jsx
+++ src/components/disease.jsx
@@ -37,25 +37,27 @@ const Disease = () => {
   }
   return (
     <div className="wrapper img_drop df">
-      <h1>Disease Prediction</h1>
-      {!previewUrl && <div className="drop_zone df" onDragOver={handleDragOver} onDrop={handleOnDrop} onClick={() => fileInput.current.click()}>
+
+      {!previewUrl && <><h1>Disease Prediction</h1><div className="drop_zone df" onDragOver={handleDragOver} onDrop={handleOnDrop} onClick={() => fileInput.current.click()}>
         <p>Drag and drop image ....</p>
-        <input type="file" accept='image/*' ref={fileInput} hidden onChange={e => handleFile(e.target.files[0])} /></div>}
-      {previewUrl && <div className="card mb-3">
-          <div className="row g-0">
-            <div className="col-md-4">
-              <img src={previewUrl} alt='image' id='img' width={"200px"} height={"200px"} />
+        <input type="file" accept='image/*' ref={fileInput} hidden onChange={e => handleFile(e.target.files[0])} /></div></>}
+      {previewUrl &&
+        <>
+          <div className='card2'></div>
+          <div class="card fx">
+            <div class="card-block fx">
+              <h1 class="card-title">Card title</h1>
+              <h6 class="card-subtitle text-muted">Support card subtitle</h6>
             </div>
-            <div className="col-md-8">
-              <div className="card-body">
-                <h5 className="card-title">Card title</h5>
-                <p className="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-                <p className="card-text"><small className="text-muted">Last updated 3 mins ago</small></p>
-              </div>
+            <img src={previewUrl} alt='image' width={"200px"} height={"200px"} />
+            <div class="card-block fx">
+              <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
             </div>
           </div>
-        </div>}
+        </>
+      }
     </div>
+
   )
 }
 
