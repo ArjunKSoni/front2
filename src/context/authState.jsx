ebd921bde5cc3d6947a344fee1f2caae5cb342e7
ArjunKSoni <aksoni0520@gmail.com>
2023-03-31 17:06:13 +0530

    .

--- src/context/authState.jsx
+++ src/context/authState.jsx
@@ -22,7 +22,7 @@ const Authstate = (props) => {
     }
 
     const search = async (search) => {
-        const apicall = await fetch(`http://127.0.0.1:3000/nav`, {
+        const apicall = await fetch(`http://127.0.0.1:5000/nav`, {
             method: 'POST',
             headers: {
                 "Content-Type": "application/json"
